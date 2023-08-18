6c87ecf7c3e3b34017ccb3977acd9b4b7dc914dc
Avinash Kumar <akgpcamp@gmail.com>
2023-08-18 21:02:00 +0530

    Microservices Init

---
+++ src/main/java/com/springboot/limitsservice/MocroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MocroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/limitsservice/MocroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
